f6816a6b0a799de10c978dab3890ce883cdeed09
Ethan Oliver <ethanliamoliver28@gmail.com>
2023-08-31 16:18:45 +0200

    fixed frontend2

--- fullstack/capstone/src/views/AboutView.vue
+++ fullstack/capstone/src/views/AboutView.vue
@@ -1,5 +1,53 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container-fluid">
+      <div class="heading">
+    <h1 class="h-01">AboutUs</h1>
+    </div>
+    
+    <div class="hero">
+      <img src="https://i.postimg.cc/wjFvCrjG/Screenshot-2023-08-31-154203.png" alt="" class="hero-img">
+    <h2 class="hero-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum facere ratione veritatis officia eaque quidem necessitatibus ad recusandae nihil tempore, repellat, doloribus, obcaecati rerum similique consectetur exercitationem. Quos, ratione facilis!</h2>
+    </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+.about {
+  background-color: #312e81 !important;
+}
+
+.h-01 {
+  position: relative;
+  right: 35%;
+  color: white;
+}
+
+.hero-img{
+  position: relative;
+  right: 27%;
+  object-fit: cover;
+  clip-path: polygon(20% 0%, 100% 0%, 80% 100%, 0% 100%);
+}
+
+.hero-text {
+  left: 42%;
+  position: absolute;
+  color: #fff;
+  font-size: 2rem; 
+  bottom: 20%;
+}
+
+.hero {
+  position: relative;
+}
+
+</style>
