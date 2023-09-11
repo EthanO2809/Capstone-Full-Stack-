7f1c4bb26187a720ffedc730427a80458f54e60b
EthanO2809 <130046659+EthanO2809@users.noreply.github.com>
2023-09-11 09:29:45 +0200

    Update Users.js

--- api/model/Users.js
+++ api/model/Users.js
@@ -46,8 +46,8 @@ class Users {
           msg: "You are providing the wrong email or password",
         });
       } else {
-        const hased = result[0].UserPass
-        await compare(UserPass, hased, (cerr, cresult) => {
+        const hashed = result[0].UserPass
+        await compare(UserPass, hashed, (cerr, cresult) => {
           if (cerr) throw cerr;
           // Create a token
           const token = createToken({
